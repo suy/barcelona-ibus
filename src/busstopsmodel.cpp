b69673e00b3be8c8f893a4b92962f437cc66ead9
Alejandro Exojo <suy@badopi.org>
2014-02-17 21:12:29 +0100

    Read data from file on startup

--- src/busstopsmodel.cpp
+++ src/busstopsmodel.cpp
@@ -1,16 +1,43 @@
+#include <QSettings>
+
 #include "busstopsmodel.h"
 
 BusStopsModel::BusStopsModel(QObject *parent) :
     QAbstractListModel(parent)
 {
-    m_stops = {
-        {"Vall d'Hebron", "1019", ""},
-        {"Vall d'Hebron, 60", "1019", "60"},
-        {"Vall d'Hebron, 73", "1019", "73"},
-        {"Rotonda de Bellesguard", "71", "60"},
-        {"", "0534", ""},
-        {"", "0534", "17"},
-    };
+    // Some default values for testing
+    // m_stops = {
+    //     {"Vall d'Hebron", "1019", ""},
+    //     {"Vall d'Hebron, 60", "1019", "60"},
+    //     {"Vall d'Hebron, 73", "1019", "73"},
+    //     {"Rotonda de Bellesguard", "71", "60"},
+    //     {"", "0534", ""},
+    //     {"", "0534", "17"},
+    // };
+
+    QSettings settings;
+
+    // settings.beginWriteArray("stops");
+    // for (int i=0; i < m_stops.size(); ++i) {
+    //     settings.setArrayIndex(i);
+    //     settings.setValue("name", m_stops.at(i).name);
+    //     settings.setValue("stop", m_stops.at(i).stop);
+    //     settings.setValue("bus",  m_stops.at(i).bus);
+    //     qDebug("loop", i);
+    // }
+    // settings.endArray();
+
+    int size = settings.beginReadArray("stops");
+    for (int i=0; i < size; ++i) {
+        settings.setArrayIndex(i);
+        BusStop stop = {
+            settings.value("name").toString(),
+            settings.value("stop").toString(),
+            settings.value("bus").toString(),
+        };
+        m_stops.append(stop);
+    }
+
 }
 
 QVariant BusStopsModel::data(const QModelIndex& index, int role) const
